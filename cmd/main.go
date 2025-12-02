241db09ac5bcd1446ab5ddbe136928f1fc55cd0a
genekkion <genekkion@live.com>
2025-12-02 08:28:06 +0000

    setup environment configuration and database initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"greddit/internal/env"
+	"greddit/internal/infra/db/postgres"
+	"greddit/internal/infra/log"
+	"log/slog"
+	"os"
+)
+
+var (
+	pgConnStr = env.GetStringEnvOrFatal("PGSQL_CONN_STR")
+)
 
 func main() {
-	fmt.Println("Hello World")
+	defer log.CleanupDefaultLogger()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	logger := log.NewLogger(log.NewHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	pool, err := postgres.New(ctx, pgConnStr)
+	if err != nil {
+		logger.Error("Error creating postgres pool",
+			"error",
+			err,
+		)
+		os.Exit(exitDbFailure)
+	}
+	defer pool.Close()
+
+	err = postgres.Init(pool, ctx)
+	if err != nil {
+		logger.Error("Error initializing postgres pool",
+			"error",
+			err,
+		)
+		os.Exit(exitDbFailure)
+	}
+
+	select {}
 }
