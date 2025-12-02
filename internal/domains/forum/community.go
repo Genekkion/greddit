7ef00fd17e1c969dd3fcf86fe2e9801900578e1c
genekkion <genekkion@live.com>
2025-12-02 04:18:54 +0000

    refactor(domain): restructure `Community` model with `CommunityValue` and `CommunityMetadata`, add `Validate` method for improved validation

--- internal/domains/forum/community.go
+++ internal/domains/forum/community.go
@@ -20,11 +20,57 @@ type CommunityId uuid.UUID
 type Community struct {
 	shared.Base
 
-	Id CommunityId `json:"id"`
+	CommunityValue
+	CommunityMetadata
+}
 
+// CommunityValue represents the value of a community.
+type CommunityValue struct {
 	Name        string `json:"name"`
 	Description string `json:"description"`
-	MemberCount int    `json:"member_count"`
+}
+
+// Validate checks that the community value is valid.
+func (v CommunityValue) Validate() error {
+	{
+
+		name := v.Name
+		name = strings.TrimSpace(name)
+		if name == "" {
+			return InvalidCommunityParamsError{
+				reason: "name cannot be empty",
+			}
+		} else if len(name) > communityMaxNameLength {
+			return InvalidCommunityParamsError{
+				reason: fmt.Sprintf("name must be less than %d characters", communityMaxNameLength),
+			}
+		}
+
+		r := []rune(name)[0]
+		if !unicode.IsLetter(r) {
+			return InvalidCommunityParamsError{
+				reason: "name must start with a letter",
+			}
+		}
+	}
+
+	{
+
+		description := v.Description
+		description = strings.TrimSpace(description)
+		if len(description) > communityMaxDescriptionLength {
+			return InvalidCommunityParamsError{
+				reason: fmt.Sprintf("description must be less than %d characters", communityMaxDescriptionLength),
+			}
+		}
+	}
+
+	return nil
+}
+
+type CommunityMetadata struct {
+	Id          CommunityId `json:"id"`
+	MemberCount int         `json:"member_count"`
 }
 
 // InvalidCommunityParamsError represents an error when creating a community with invalid parameters.
@@ -38,37 +84,19 @@ func (e InvalidCommunityParamsError) Error() string {
 }
 
 // NewCommunity creates a new community.
-func NewCommunity(name string, description string, base shared.Base) (community *Community, err error) {
-	name = strings.TrimSpace(name)
-	if name == "" {
-		return nil, InvalidCommunityParamsError{
-			reason: "name cannot be empty",
-		}
-	} else if len(name) > communityMaxNameLength {
-		return nil, InvalidCommunityParamsError{
-			reason: fmt.Sprintf("name must be less than %d characters", communityMaxNameLength),
-		}
+func NewCommunity(value CommunityValue, metadata CommunityMetadata, base shared.Base) (community *Community, err error) {
+	err = value.Validate()
+	if err != nil {
+		return nil, err
 	}
 
-	r := []rune(name)[0]
-	if !unicode.IsLetter(r) {
-		return nil, InvalidCommunityParamsError{
-			reason: "name must start with a letter",
-		}
-	}
-
-	description = strings.TrimSpace(description)
-	if len(description) > communityMaxDescriptionLength {
-		return nil, InvalidCommunityParamsError{
-			reason: fmt.Sprintf("description must be less than %d characters", communityMaxDescriptionLength),
-		}
-	}
+	metadata.MemberCount = 0
 
 	community = &Community{
 		Base: base,
 
-		Name:        name,
-		Description: description,
+		CommunityValue:    value,
+		CommunityMetadata: metadata,
 	}
 
 	return community, nil
